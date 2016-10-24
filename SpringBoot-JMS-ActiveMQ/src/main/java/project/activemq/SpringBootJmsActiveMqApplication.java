e7f126ae5aca73207eafc32a071a7903a51939b3
nocnisetac <alexaq007@gmail.com>
2016-10-24 23:48:08 +0200

    ActiveMQ

---
+++ SpringBoot-JMS-ActiveMQ/src/main/java/project/activemq/SpringBootJmsActiveMqApplication.java
@@ -0,0 +1,12 @@
+package project.activemq;
+//Note
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootJmsActiveMqApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootJmsActiveMqApplication.class, args);
+	}
+}
